8cef54ee9af6fe3542015fcd61979ea4d3ecb702
Denver <70090559+Denver44@users.noreply.github.com>
2021-10-01 06:26:40 +0530

    useEffect Hook Fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
+
+// import State1 from "./Components/State1";
+// import State2 from "./Components/State2";
+// import State3 from "./Components/State3";
 import State4 from "./Components/State4";
 
-const App = () => <State4 />;
+const App = () => {
+  return (
+    <div>
+      {/* <State1 /> */}
+      {/* <State2 /> */}
+      {/* <State3 /> */}
+      <State4 />
+    </div>
+  );
+};
 export default App;
