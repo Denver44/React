309667bd071731c80a4cd1e93a4b529b7f9d385f
Denver <70090559+Denver44@users.noreply.github.com>
2021-12-02 20:30:10 +0530

    react-router-dom package installed and implemented

--- src/App.js
+++ src/App.js
@@ -3,62 +3,49 @@ import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
-import Route from "./components/Route";
 import Header from "./components/Header";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { items, options } from "./utils/helper";
 
-const items = [
-  {
-    title: "What is React?",
-    content: "React is a front end javascript framework",
-  },
-  {
-    title: "Why use React?",
-    content: "React is a favorite JS library among engineers",
-  },
-  {
-    title: "How do you use React?",
-    content: "You use React by creating components",
-  },
-];
-
-const options = [
-  {
-    label: "The Color Red",
-    value: "red",
-  },
-  {
-    label: "The Color Green",
-    value: "green",
-  },
-  {
-    label: "A Shade of Blue",
-    value: "blue",
-  },
-];
+const MyDropdown = ({ options, selected, setSelected }) => {
+  return (
+    <Dropdown
+      {...{
+        options,
+        selected,
+      }}
+      label="Select a color"
+      onSelectedChange={setSelected}
+    />
+  );
+};
 
 const App = () => {
   const [selected, setSelected] = useState(options[0]);
 
   return (
     <div>
-      <Header />
-      <Route path="/">
-        <Accordion items={items} />
-      </Route>
-      <Route path="/list">
-        <Search />
-      </Route>
-      <Route path="/dropdown">
-        <Dropdown
-          label="Select a color"
-          options={options}
-          selected={selected}
-          onSelectedChange={setSelected}
-        />
-      </Route>
-      <Route path="/translate">
-        <Translate />
-      </Route>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact>
+            <Accordion items={items} />
+          </Route>
+          <Route path="/list">
+            <Search />
+          </Route>
+          <Route path="/dropdown">
+            <MyDropdown
+              options={options}
+              selected={selected}
+              setSelected={setSelected}
+            />
+          </Route>
+          <Route path="/translate">
+            <Translate />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
