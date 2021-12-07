e76ab66362e42ab7b34551e0b4c175c5e82bb4a0
Denver <70090559+Denver44@users.noreply.github.com>
2021-12-07 14:25:51 +0530

    Cheerio Queries

--- src/RootProvider.js
+++ src/RootProvider.js
@@ -2,8 +2,10 @@ import { Provider } from "react-redux";
 import { createStore } from "redux";
 import reducers from "reducers";
 
-const RootProvider = ({ children }) => {
-  return <Provider store={createStore(reducers, {})}>{children}</Provider>;
+const RootProvider = ({ children, initialState = {} }) => {
+  return (
+    <Provider store={createStore(reducers, initialState)}>{children}</Provider>
+  );
 };
 
 export default RootProvider;
--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -1,31 +1,30 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
-function CommentList() {
-  const commentList = useSelector((state) => state.comments);
+const ListStyle = {
+  display: "block",
+  margin: "5px",
+  padding: "8px",
+  border: "1px solid gray",
+  backgroundColor: "#eef",
+  fontSize: "14px",
+  fontWeight: "500",
+  fontFamily: "sans serif",
+};
 
-  return (
-    <ul>
-      {commentList.map((comment, id) => {
-        return (
-          <li
-            key={id}
-            style={{
-              display: "block",
-              margin: "5px",
-              padding: "5px",
-              border: "1px solid gray",
-              backgroundColor: "#eef",
-              fontSize: "16px",
-              fontWeight: "500",
-            }}
-          >
-            {comment}
-          </li>
-        );
-      })}
-    </ul>
-  );
-}
+const RenderList = ({ commentList }) => {
+  return commentList.map((comment, id) => {
+    return (
+      <li key={id} style={ListStyle}>
+        {comment}
+      </li>
+    );
+  });
+};
+
+const CommentList = () => {
+  const commentList = useSelector((state) => state.comments);
+  return <RenderList commentList={commentList} />;
+};
 
 export default CommentList;
