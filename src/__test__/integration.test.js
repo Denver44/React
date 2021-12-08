bb48a481e1e7888cdc8dfbdc587efdaad3bc0bd4
Denver <70090559+Denver44@users.noreply.github.com>
2021-12-08 10:41:08 +0530

    Integratiion test completed

--- src/__test__/integration.test.js
+++ src/__test__/integration.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import { mount } from "enzyme";
 import App from "App";
 import RootProvider from "RootProvider";
+import moxios from "moxios";
 
 let wrapped;
 beforeEach(() => {
@@ -10,10 +11,27 @@ beforeEach(() => {
       <App />
     </RootProvider>
   );
+  moxios.install();
+  moxios.stubRequest("https://jsonplaceholder.typicode.com/comments", {
+    status: 200,
+    response: [{ name: "Fetched #1" }, { name: "Fetched #2" }],
+  });
 });
 
-it("can fetch a list of comments and display them", () => {
+afterEach(() => {
+  moxios.uninstall();
+  wrapped.unmount();
+});
+
+it("can fetch a list of comments and display them", (done) => {
   // Attempt to render the "entire APP"
   // Find the 'Fetch Comments' button and click it
+
+  wrapped.find(".fetchComment").simulate("click");
+  moxios.wait(() => {
+    wrapped.update();
+    expect(wrapped.find("li").length).toEqual(2);
+    done();
+  });
   //   Expect to find a list of comments!
 });
