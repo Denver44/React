f3f2569194eabf0f3680e171384608cfb70a6fff
Denver <70090559+Denver44@users.noreply.github.com>
2022-01-11 14:16:12 +0530

    React portal Done

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,11 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const ModalStyles = {
+  position: 'fixed',
+  top: '40%',
+  left: '20%',
+  transform: 'translate(-50%,-50%)',
+  zIndex: 1000,
+};
 
 const Modal = ({ onDismiss, title, content, actions }) => {
   return ReactDOM.createPortal(
-    <div>
+    <div style={ModalStyles}>
       <div
-        onCLick={(e) => e.stopPropagation()}
+        onClick={(e) => e.stopPropagation()}
         className="ui standard modal visible active"
       >
         <button onClick={() => onDismiss()}>
@@ -16,8 +24,12 @@ const Modal = ({ onDismiss, title, content, actions }) => {
         <div className="actions">{actions}</div>
       </div>
     </div>,
-    document.querySelector("section")
+    document.querySelector('section')
   );
 };
 
 export default Modal;
+
+// So if we want to creatre a modal or tool tip which can render on someoneone then alaways use createPortal it will give the function to click on it render wwill not give this function
+
+// onClick={(e) => e.stopPropagation()} this will helps us that event don't get bubble up and go to correct div
