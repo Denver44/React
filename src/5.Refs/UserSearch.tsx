cfa80ca7001688c33755bb82962e3b98bc7c5d40
Denver <70090559+Denver44@users.noreply.github.com>
2022-01-28 03:12:45 +0530

    #1zw22kw Types Around Events and Refs done

---
+++ src/5.Refs/UserSearch.tsx
@@ -0,0 +1,49 @@
+import { useState, useRef, useEffect } from 'react';
+
+const users = [
+  { name: 'Sarah', age: 20 },
+  { name: 'Alex', age: 20 },
+  { name: 'Michael', age: 20 },
+];
+
+const UserSearch: React.FC = () => {
+  // Whenever we are using ref we have to declare the type and put null in or and another thing is that we have to initialize the ref initial value as null.
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const [name, setName] = useState('');
+  const [user, setUser] = useState<{ name: string; age: number } | undefined>();
+
+  useEffect(() => {
+    // This type of type guard ios necessary when we are dealing with use ref.
+    if (!inputRef.current) {
+      return;
+    }
+
+    inputRef.current.focus();
+  }, []);
+
+  const onClick = () => {
+    const foundUser = users.find((user) => {
+      return user.name === name;
+    });
+
+    setUser(foundUser);
+  };
+
+  return (
+    <div>
+      User Search
+      <input
+        ref={inputRef}
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={onClick}>Find User</button>
+      <div>
+        {user && user.name}
+        {user && user.age}
+      </div>
+    </div>
+  );
+};
+
+export default UserSearch;
